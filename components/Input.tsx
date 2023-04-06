f4eb7d1aaad9e4ce8a95fb50869397ec5fd3470c
Annie <anniehartmann97@gmail.com>
2023-04-06 21:39:41 +0300

    Sellers page

--- components/Input.tsx
+++ components/Input.tsx
@@ -7,30 +7,39 @@ interface IInput {
   placeholder: string;
   label?: string;
   isLabel?: boolean;
+  page?: string;
 }
 
-const Input = ({ type, placeholder, label, isLabel }: IInput) => {
+const Input = ({ type, placeholder, label, isLabel, page }: IInput) => {
   return (
     <>
       {isLabel && <InputLabel>{label}</InputLabel>}
-      <InputWrap type={type} placeholder={placeholder} />
+      <InputWrap page={page} type={type} placeholder={placeholder} />
     </>
   );
 };
 
-const InputWrap = styled.input<{ type: string }>`
+const InputWrap = styled.input<{ type: string; page?: string }>`
   ${(props) => {
     if (props.type === "search") {
       return `
         border: none;
         height: 100%;
         font-weight: 400;
-        font-size: ${rem(20)};
-        line-height: ${rem(24)};
-        color: ${rgba(props.theme.colors.white, 1)};
+        font-size:  ${props.page == "intro" ? rem(16) : rem(20)};
+        line-height: ${props.page == "intro" ? rem(19) : rem(24)} ;
+        color: ${
+          props.page == "intro"
+            ? props.theme.colors.gray.default
+            : rgba(props.theme.colors.white, 0.5)
+        };
         &::placeholder {
           position: relative;
-          color: ${rgba(props.theme.colors.white, 0.5)};
+          color: ${
+            props.page == "intro"
+              ? props.theme.colors.gray.default
+              : rgba(props.theme.colors.white, 0.5)
+          };
         }
       `;
     } else {
@@ -38,7 +47,7 @@ const InputWrap = styled.input<{ type: string }>`
         font-size: ${rem(16)};
         line-height: ${rem(19)};
         padding: ${rem(14)} ${rem(16)};
-        background: #FFFFFF;
+        background: ${props.theme.colors.white};
         border: 1px solid #EFF0F2;
         border-radius: ${rem(12)};
         &::placeholder {
@@ -50,6 +59,7 @@ const InputWrap = styled.input<{ type: string }>`
   width: 100%;
   background: none;
   outline: none;
+  font-family: "PT Root UI", Arial, sans-serif;
 `;
 
 const InputLabel = styled.label`
